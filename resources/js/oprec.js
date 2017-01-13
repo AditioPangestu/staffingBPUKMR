dfe10cbd398e28e2a8c233e0a5f467d9ee2de4bc
Aditio Pangestu <aditiopangestu@gmail.com>
2017-01-13 10:04:12 +0700

    Staffing Start Pack

---
+++ resources/js/oprec.js
@@ -0,0 +1,50 @@
+$(document).ready(function(){
+   $("input[name=pertama]") // select the radio by its id
+    .change(function(){ // bind a function to the change event
+        if( $(this).is(":checked") ){ // check if the radio is checked
+            var pertamaString = $(this).val(); // retrieve the value
+            var keduaButton = $("input[name=kedua]:checked");
+            var ketigaButton = $("input[name=ketiga]:checked");
+            if (keduaButton) {
+                if (pertamaString === keduaButton.val())
+                    keduaButton.prop('checked',false);
+            }
+            if (ketigaButton) {
+                if (pertamaString === ketigaButton.val())
+                    ketigaButton.prop('checked',false);
+            }
+        }
+    });
+    $("input[name=kedua]") // select the radio by its id
+    .change(function(){ // bind a function to the change event
+        if( $(this).is(":checked") ){ // check if the radio is checked
+            var keduaString = $(this).val(); // retrieve the value
+            var pertamaButton = $("input[name=pertama]:checked");
+            var ketigaButton = $("input[name=ketiga]:checked");
+            if (pertamaButton) {
+                if (keduaString === pertamaButton.val())
+                    pertamaButton.prop('checked',false);
+            }
+            if (ketigaButton) {
+                if (keduaString === ketigaButton.val())
+                    ketigaButton.prop('checked',false);
+            }
+        }
+    });
+    $("input[name=ketiga]") // select the radio by its id
+    .change(function(){ // bind a function to the change event
+        if( $(this).is(":checked") ){ // check if the radio is checked
+            var ketigaString = $(this).val(); // retrieve the value
+            var pertamaButton = $("input[name=pertama]:checked");
+            var keduaButton = $("input[name=kedua]:checked");
+            if (pertamaButton) {
+                if (ketigaString === pertamaButton.val())
+                    pertamaButton.prop('checked',false);
+            }
+            if (keduaButton) {
+                if (ketigaString === keduaButton.val())
+                    keduaButton.prop('checked',false);
+            }
+        }
+    });
+});
